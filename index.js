49c9eadba87b0d10c6ca2793c9e907c3a49e8d37
Alejandro <arizaguirre19@gmail.com>
2023-08-16 22:45:37 -0600

    Agregada la carpeta de modules al backend y las rutas del RestAPI ya estan hechas

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express')
 const app = express()
 const port = 3000
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 
 /*Este es un objeto de prueba*/
 var motoristas = [{
@@ -26,5 +28,36 @@ app.post('/motoristas', function(req, res){
     foto: req.body.foto
     }
     motoristas.push(motorista);
-    res.send({mensaje:'Registro guardado', motoristaGuardado: motorista});
+    res.send({CodigoResultado:1, mensaje:'Registro guardado', motoristaGuardado: motorista});
+})
+
+//obtener un solo motorista
+app.get('/motoristas/:id', function(req, res){
+    if(req.params.id > (motoristas.length-1)){
+        res.send({CodigoResultado:0, mensaje:"Motorista no existe en los registros"});
+    }else{
+        res.send(motoristas[req.params.id]);
+    }
+})
+
+//obtener todos los motoristas
+app.get('/motoristas', function(req, res){
+    res.send(motoristas);
+})
+
+//actualizar un motorista
+app.put('/motoristas/:id', function(req, res){
+    let motorista = {
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+        foto: req.body.foto
+        }
+
+    motoristas[req.params.id] = motorista;
+    res.send({CodigoResultado:1, mensaje:"Motorista actualizado"})
+})
+
+app.delete('/motoristas/:id', function(req, res){
+    motoristas.splice(req.params.id, 1); //splice recibe el indeice y el elemento a borrar
+    res.send({CodigoResultado:1, mensaje:"Motorista eliminado"})
 })
