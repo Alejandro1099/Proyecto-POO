14eb3701758113d801c1d28a1702b59e7fc8eb18
Alejandro <arizaguirre19@gmail.com>
2023-08-26 23:02:51 -0600

    Ultimo cambio

--- Backend-App/routes/usuario-router.js
+++ Backend-App/routes/usuario-router.js
@@ -3,20 +3,34 @@ var router = express.Router();
 var usuario = require('../models/usuario');
 
 //registrar un nuevo usuario
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
     let cliente = new usuario(
         {
             nombre: req.body.nombre,
             pass: req.body.pass
         }
     );
-    cliente.save().then(result=>{
+    cliente.save().then(result => {
         res.send('Usuario registrado con exito');
         res.end();
-    }).catch(error=>{
+    }).catch(error => {
         res.send(error);
         res.end();
     })
+});
+
+// Ruta para validar un usuario
+router.post('/validate-user', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Buscar al usuario en la base de datos
+    const user = await usuarios.findOne({ nombre, pass });
+
+    if (user) {
+        res.json({ message: 'Usuario válido' });
+    } else {
+        res.status(401).json({ message: 'Credenciales inválidas' });
+    }
 });
 
 module.exports = router;
--- Frotend-App/src/js/controlador_usuario.js
+++ Frotend-App/src/js/controlador_usuario.js
@@ -1,21 +1,21 @@
 let empresas_lista;
 let lista_productos;
 
-const cargarEmpresas = async()=>{
+const cargarEmpresas = async () => {
     const respuesta = await fetch('http://localhost:3000/empresas', {
-        method:'GET',
-        headers:{
-            'Content-Type':'application/json'
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
     });
     empresas_lista = await respuesta.json();
     console.log("lista de empresas ", empresas_lista);
 }
 
-const renderizarEmpresas = ()=>{
+const renderizarEmpresas = () => {
     document.getElementById("contenido").innerHTML = "";
     empresas_lista.forEach(empresa => {
-        document.getElementById("contenido").innerHTML+=`
+        document.getElementById("contenido").innerHTML += `
         <div class="card" style="width: 15rem;" id="card_empresa">
             <img src="${empresa.foto}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -26,18 +26,18 @@ const renderizarEmpresas = ()=>{
     });
 }
 
-cargarEmpresas().then(()=>{
+cargarEmpresas().then(() => {
     renderizarEmpresas()
-    }).catch((error)=>{
-        console.log(`Error al obtener las empresas ${error}`)
+}).catch((error) => {
+    console.log(`Error al obtener las empresas ${error}`)
 });
 
 
 
 //registrar un nuevo usuario
-document.getElementById("form_registro").addEventListener("submit", function(event){
+document.getElementById("form_registro").addEventListener("submit", function (event) {
     event.preventDefault();//evita que se envie la informacion del formulario a otro sitio
-    
+
     const nombre = document.getElementById("name").value;
     const contra = document.getElementById("pass").value;
 
